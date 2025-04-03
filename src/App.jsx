334b87711b80fef1b0cfbb957d3143bb6bb101dd
drakkken <lisbj456@gmail.com>
2025-04-03 18:17:36 +0530

    Initial-Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Button from "./components/Button";
+import Header from "./components/Header";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Hero from "./components/Hero";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+          <Header />
+          <Hero />
+        </div>
+        <ButtonGradient />
+      </div>
+    </>
+  );
+};
+
+export default App;
