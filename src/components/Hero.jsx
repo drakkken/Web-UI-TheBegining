f9d0f0e895f0bd23bb7a680fde5c407ffa653737
drakkken <lisbj456@gmail.com>
2025-04-03 23:12:33 +0530

    Initial-Commit3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,48 +1,126 @@
-import React from "react";
-import Section from "./Section";
+import { curve, heroBackground, robot } from "../assets";
 import Button from "./Button";
-import curve from "../assets/hero/curve.png";
+import Section from "./Section";
+import { BackgroundCircles, BottomLine, Gradient } from "./design/Hero";
+import { heroIcons } from "../constants";
+import { ScrollParallax } from "react-just-parallax";
+import { useRef } from "react";
+import Generating from "./Generating";
+import Notification from "./Notification";
+import CompanyLogos from "./CompanyLogos";
 
 const Hero = () => {
+  const parallaxRef = useRef(null);
+
   return (
-    <div>
-      <Section
-        className="pt-[12rem] -mt-[5.25] "
-        crosses
-        crossesOffset="lg:translate-y-[5.25rem]"
-        customPaddings
-        id="hero"
-      >
-        <div className=" hover:border container relative">
-          <div
-            className=" hover:border relative z-1 max-w-[62rem]
-          mx-auto text-center mb-[4rem] md:mb-[20] lg:mb-[6rem]"
-          >
-            <h1 className="h1 mb-6">
-              Explore The Possibliteis of AI Chatting With{" "}
-              <span className="inline-block relative">
-                BrainWave
-                <img
-                  src={curve}
-                  className="absolute top-full  w-full left-0 xl:-mt-2 "
-                  width={624}
-                  height={28}
-                  alt="Curve"
-                />
-              </span>
-            </h1>
-            <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
-              Unleash the power of AI within brainwave
-            </p>
-          </div>
-          <div className="hover:border flex items-center justify-center">
-            <Button href="/pricing" white>
-              Get Started
-            </Button>
-          </div>
+    <Section
+      className="pt-[12rem] -mt-[5.25rem] overflow-hidden"
+      crosses
+      crossesOffset="lg:translate-y-[5.25rem]"
+      customPaddings
+      id="hero"
+    >
+      <div className="container relative overflow-hidden" ref={parallaxRef}>
+        {/* Hero Text */}
+        <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
+          <h1 className="h1 mb-6">
+            Explore the Possibilities of&nbsp;AI&nbsp;Chatting with{" "}
+            <span className="inline-block relative">
+              Brainwave{" "}
+              <img
+                src={curve}
+                className="absolute top-full left-0 w-full xl:-mt-2"
+                width={624}
+                height={28}
+                alt="Curve"
+              />
+            </span>
+          </h1>
+          <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+            Unleash the power of AI within Brainwave. Upgrade your productivity
+            with Brainwave, the open AI chat app.
+          </p>
+          <Button href="/pricing" white>
+            Get started
+          </Button>
         </div>
-      </Section>
-    </div>
+
+        {/* Parallax Section */}
+        <div className="relative max-w-[23rem] mx-auto md:max-w-5xl xl:mb-24">
+          {/* Background Gradient Box */}
+          <div className="relative z-1 p-0.5 rounded-2xl bg-conic-gradient">
+            <div className="relative bg-n-8 rounded-[1rem]">
+              <div className="h-[1.4rem] bg-n-10 rounded-t-[0.9rem]" />
+
+              {/* Robot Image Parallax */}
+              <div className="aspect-[33/40] rounded-b-[0.9rem] overflow-hidden md:aspect-[688/490] lg:aspect-[1024/490]">
+                <ScrollParallax strength={0.4} translateY={[20, -20]}>
+                  <img
+                    src={robot}
+                    className="w-full scale-[1.7] translate-y-[8%] md:scale-[1] md:-translate-y-[10%] lg:-translate-y-[23%]"
+                    width={1024}
+                    height={490}
+                    alt="AI"
+                  />
+                </ScrollParallax>
+
+                {/* Generating Animation */}
+                <Generating className="absolute left-4 right-4 bottom-5 md:left-1/2 md:right-auto md:bottom-8 md:w-[31rem] md:-translate-x-1/2" />
+
+                {/* Parallax Moving Icons */}
+                <ScrollParallax
+                  isAbsolutelyPositioned
+                  strength={0.3}
+                  translateY={[30, -30]}
+                  scrollContainerRef={parallaxRef}
+                >
+                  <ul className="hidden absolute -left-[5.5rem] bottom-[7.5rem] px-1 py-1 bg-n-9/40 backdrop-blur border border-n-1/10 rounded-2xl xl:flex">
+                    {heroIcons.map((icon, index) => (
+                      <li className="p-5" key={index}>
+                        <img src={icon} width={24} height={25} alt="icon" />
+                      </li>
+                    ))}
+                  </ul>
+                </ScrollParallax>
+
+                {/* Parallax Moving Notification */}
+                <ScrollParallax
+                  isAbsolutelyPositioned
+                  strength={0.5}
+                  translateY={[40, -40]}
+                  scrollContainerRef={parallaxRef}
+                >
+                  <Notification
+                    className="hidden absolute -right-[5.5rem] bottom-[11rem] w-[18rem] xl:flex"
+                    title="Code generation"
+                  />
+                </ScrollParallax>
+              </div>
+            </div>
+
+            <Gradient />
+          </div>
+
+          {/* Background Image */}
+          <div className="absolute -top-[54%] left-1/2 w-[234%] -translate-x-1/2 md:-top-[46%] md:w-[138%] lg:-top-[104%]">
+            <img
+              src={heroBackground}
+              className="w-full"
+              width={1440}
+              height={1800}
+              alt="hero"
+            />
+          </div>
+
+          <BackgroundCircles />
+        </div>
+
+        {/* Company Logos */}
+        <CompanyLogos className="hidden relative z-10 mt-20 lg:block" />
+      </div>
+
+      <BottomLine />
+    </Section>
   );
 };
 
